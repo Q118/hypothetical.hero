a0d8f093dc93004a887f637818f108d46435b81b
Q118 <shelbyfish91@gmail.com>
2021-04-03 16:40:10 -0700

    touch childhood set up logic

---
+++ toughChildhood.js
@@ -0,0 +1,26 @@
+/** @format */
+function analyze(params) {
+	analyze(params);
+	//? return params
+}
+function toughChildhood(hero) {
+	const life = {};
+	life.name = hero;
+	life.environmentalFactors = [];
+	life.stuffBornWith = [];
+	life.theme = function () {
+		while (age < 18) {
+			analyze(
+				cliffNotes(life.experiences),
+				life.stuffBornWith,
+				life.environmentalFactors
+			);
+		}
+	};
+	const herosChildhood = life.analyze(
+		environmentalFactors,
+		stuffBornWith,
+		theme
+	);
+	return herosChildhood;
+}
