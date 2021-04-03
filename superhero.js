056078dd8d6b5d5df8f1ab0dc38bf6843330cf44
Q118 <shelbyfish91@gmail.com>
2021-04-03 16:38:44 -0700

    superhero object prototype

---
+++ superhero.js
@@ -0,0 +1,17 @@
+/** @format */
+
+const Hero = [
+	birth,
+	toughChildhood,
+	characterBuildingAdversary,
+	"some accident/event/incident that makes powers appear",
+	"the struggle for society to see them for a hero",
+	"a bad guy comes in to the story disguised as good",
+	"our new hero is outcasted/pulledaway/lost in some way",
+	"then the bad stufdf happens from the bad guy and it may be to late",
+	"the hero goes theough something hard like the death of *thier* hero or...",
+	"hero pushes through his pain and gets stronger",
+	"then return of our hero and saves the day",
+	"some type of event/situation/dialgoue to signify there is more to the story and to be continued with edge on our seats",
+	"ending credits",
+];
